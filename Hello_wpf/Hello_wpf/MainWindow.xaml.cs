54664ecd6d63bf6d76160f1c3bc68566c49f2382
bood43v <budaevgena@gmail.com>
2023-05-13 22:08:29 +0900

    Добавьте файлы проекта.

---
+++ Hello_wpf/Hello_wpf/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hello_wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        bool _shown;
+
+        static string alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+        protected override void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+
+            if (_shown)
+                return;
+
+            _shown = true;
+
+            for (int i = 1; i <= 33; i++)
+            {
+                ComboBoxShift.Items.Add(i);
+            }
+        }
+
+        public static string CaesarCipherEncrypt(string cipherText, int key)
+        {
+            string cipherTextLow = cipherText.ToLower();
+            char[] sourceText = new char[cipherTextLow.Length];
+            int j = 0;
+            key = -key;
+            for (int i = 0; i < cipherTextLow.Length; i++)
+            {
+                if (!char.IsLetter(cipherTextLow[i]))
+                    sourceText[i] = cipherTextLow[i];
+                else
+                {
+                    sourceText[i] = '|';
+                    while (sourceText[i] == '|')
+                    {
+                        if (cipherTextLow[i] == alphabet[j])
+                        {
+                            try
+                            {
+                                sourceText[i] = alphabet[j - key];
+                            }
+                            catch
+                            {
+                                sourceText[i] = alphabet[(j - key) + 33];
+                            }
+                        }
+                        j++;
+                    }
+                    j = 0;
+                }
+            }
+
+            return new string(sourceText);
+        }
+
+        public static string CaesarCipherDecrypt(string cipherText, int key)
+        {
+            string cipherTextLow = cipherText.ToLower();
+            char[] sourceText = new char[cipherTextLow.Length];
+            int j = 0;
+            // key = -key;
+            for (int i = 0; i < cipherTextLow.Length; i++)
+            {
+                if (!char.IsLetter(cipherTextLow[i]))
+                    sourceText[i] = cipherTextLow[i];
+                else
+                {
+                    sourceText[i] = '|';
+                    while (sourceText[i] == '|')
+                    {
+                        if (cipherTextLow[i] == alphabet[j])
+                        {
+                            try
+                            {
+                                sourceText[i] = alphabet[j - key];
+                            }
+                            catch
+                            {
+                                sourceText[i] = alphabet[(j - key) + 33];
+                            }
+                        }
+                        j++;
+                    }
+                    j = 0;
+                }
+            }
+
+            return new string(sourceText);
+        }
+
+        private void ButtonToEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            if(ComboBoxOperation.Text == "Зашифровать")
+                TextBoxResult.Text = CaesarCipherEncrypt(TextBox1.Text, int.Parse(ComboBoxShift.SelectedItem.ToString()));
+            else
+            if(ComboBoxOperation.Text == "Расшифровать")
+                TextBoxResult.Text = CaesarCipherDecrypt(TextBox1.Text, int.Parse(ComboBoxShift.SelectedItem.ToString()));                
+        }
+
+        private void ComboBoxOperation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {   
+            if (ComboBoxOperation.Text == "Зашифровать")
+                Button.Content = "Расшифровать";
+            else 
+            if (ComboBoxOperation.Text == "Расшифровать")
+                Button.Content = "Зашифровать";
+        }
+    }
+}
---
+++ classC#/TestProject_ClassFraction/FractionTest.cs
@@ -0,0 +1,125 @@
+﻿/// Обыкновенные дроби и операции над ними
+/// Тестирование методов класса
+/// @author Будаев Г.Б.
+using classFraction;
+
+namespace TestProject_ClassFraction
+{
+    
+    [TestClass]
+    public class FractionTest
+    {
+        [TestMethod]
+        public void ConstructorsTest() /// Тест конструкторов
+        {
+            Fraction F1 = new Fraction();
+            Fraction F2 = new (3, 5);
+            Assert.AreEqual(1, F1.GetNumerator(), "Constructor w/o params error ");
+            Assert.AreEqual(1, F1.GetDenominator(), "Constructor w/o params error ");
+            Assert.AreEqual(3, F2.GetNumerator(), "Constructor w/ params error ");
+            Assert.AreEqual(5, F2.GetDenominator(), "Constructor w/ params error ");
+
+            Fraction F3 = new(-3, 1);
+            Assert.AreEqual(-3, F3.GetNumerator(), "Constructor w/ params error ");
+            Assert.AreEqual(1, F3.GetDenominator(), "Constructor w/ params error ");
+
+            /// Тест исключительной ситуации, когда знаменатель равен нулю
+            Assert.ThrowsException<ArgumentException>(() => F3.SetDenominator(0));
+        }
+
+        [TestMethod]
+        public void SetterGetterChangerTest() /// Тест сеттеров и геттеров
+        {
+            Fraction F = new Fraction();
+            F.SetNumerator(5);
+            F.SetDenominator(10);
+            Assert.AreEqual(5, F.GetNumerator(), "Setter error ");
+            Assert.AreEqual(10, F.GetDenominator(), "Setter error ");
+
+            F.ChangeParams(6, 11);
+            Assert.AreEqual(6, F.GetNumerator(), "Changer error ");
+            Assert.AreEqual(11, F.GetDenominator(), "Changer error ");
+
+            F.ChangeParams(-6, 11);
+            Assert.AreEqual(-6, F.GetNumerator(), "Changer error ");
+            Assert.AreEqual(11, F.GetDenominator(), "Changer error ");
+
+            /// Тест исключительной ситуации, когда знаменатель равен нулю
+            Assert.ThrowsException<ArgumentException>(() => F.ChangeParams(1,0));
+        }
+
+        [TestMethod]
+        public void ToStringTest() /// Тест метода перевода в строковый тип
+        {
+            Fraction F = new Fraction(1,2);
+            Assert.AreEqual("1/2", F.ToString(), "ToString error ");
+
+            Fraction F2 = new Fraction(-55, 2);
+            Assert.AreEqual("-55/2", F2.ToString(), "ToString error ");
+
+            Fraction F3 = new Fraction(12, 12);
+            Assert.AreEqual("1/1", F3.ToString(), "ToString error ");
+        }
+
+        [TestMethod]
+        public void OperatorsTest() /// Тест операторов
+        {
+            Fraction F1 = new Fraction(1, 5);
+            Fraction F2 = new Fraction(3, 5);
+            Assert.AreEqual( "4/5", (F1 + F2).ToString(), "Operator+ error ");
+            Assert.AreEqual("-2/5", (F1 - F2).ToString(), "Operator- error ");
+            Assert.AreEqual("3/25", (F1 * F2).ToString(), "Operator* error ");
+            Assert.AreEqual( "1/3", (F1 / F2).ToString(), "Operator/ error ");
+
+            Fraction F3 = new Fraction(3, 6);
+            Fraction F4 = new Fraction(1, 2);
+            Assert.AreEqual("1/1", (F3 + F4).ToString(), "Operator+ error ");
+            Assert.AreEqual("0/1", (F3 - F4).ToString(), "Operator- error ");
+            Assert.AreEqual("1/4", (F3 * F4).ToString(), "Operator* error ");
+            Assert.AreEqual("1/1", (F3 / F4).ToString(), "Operator/ error ");
+
+            Fraction F5 = new Fraction(0, 5);
+
+            /// Тест исключительной ситуации на деление на ноль
+            Assert.ThrowsException<DivideByZeroException>(() => F4/F5);
+        }
+
+        [TestMethod]
+        public void ShortenTest() /// Тест сокращения
+        {
+            Fraction F = new Fraction(2, 4);
+            Assert.AreEqual("1/2", (F.Shorten()).ToString(), "Shorten error ");
+
+            Fraction F2 = new Fraction(-2, 4);
+            Assert.AreEqual("-1/2", (F2.Shorten()).ToString(), "Shorten error ");
+
+            Fraction F3 = new Fraction(10, 5);
+            Assert.AreEqual("2/1", (F3.Shorten()).ToString(), "Shorten error ");
+
+        }
+
+        [TestMethod]
+        public void ConvertToDoubleTest() /// Тест перевода в вещественное число
+        {
+            Fraction F = new Fraction(1, 3);
+            Assert.AreEqual(0.333, F.ConvertToDouble(), 0.001, "ConvertToDouble error");
+
+            Fraction F2 = new Fraction(-5, 3);
+            Assert.AreEqual(-1.666, F2.ConvertToDouble(), 0.001, "ConvertToDouble error");
+
+            Fraction F3 = new Fraction(10, 5);
+            Assert.AreEqual(2, F3.ConvertToDouble(), 0.001, "ConvertToDouble error");
+        }
+
+        [TestMethod]
+        public void ComparisonTest() /// Тест сравнения
+        {
+            Fraction F1 = new Fraction(2, 3);
+            Fraction F2 = new Fraction(1, 3);
+            Assert.AreEqual(1, F1.Compare(F2), "Comparison error");
+            Assert.AreEqual(-1, F2.Compare(F1), "Comparison error");
+            Assert.AreEqual(0, F1.Compare(F2+F2), "Comparison error");
+        }
+
+    }
+}

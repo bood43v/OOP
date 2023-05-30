be180cac457ffb7014a151fc70259bce6a5eb6a7
bood43v <budaevgena@gmail.com>
2023-05-30 11:22:25 +0900

    шаблон

---
+++ CaesarCypher/CaesarCypher/CaesarCypher/FirstPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CaesarCypher
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_crypt(object sender, EventArgs e)
+        {
+            Editor2.Text = Editor1.Text;
+        }
+    }
+}
